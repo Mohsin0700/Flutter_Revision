3c6d6a1a5d93b3dceb6139fa33e37e7da9ba16fc
= <=>
2024-07-17 03:06:40 +0500

    Files updated

---
+++ class03_basic_data_types/main.dart
@@ -0,0 +1,51 @@
+void main(){
+  // print("Hello world");
+  // String myName;
+  // myName = "Hafiz Mohsin";
+  // //  can also be written as
+  // String name = "Hafiz Mohsin";
+  // print('Myname is $name');
+
+  // int age = 26;
+  // print(age);
+  // double percentage1 = 76.3;
+  
+  // int percentage2 = percentage1.round();  // round to nearby number...forware if greater than 5, backward if less than 5
+  // int percentage3 = percentage1.floor();  // round to backward number, doesnot matter what the decimal number is
+  // int percentage4 = percentage1.ceil();   // round to upward, does'nt matter what the number is
+  // print("My percentage is $percentage1");
+  // print("My percentage2 is $percentage2");
+  // print("My percentage2 is $percentage3");
+  // print("My percentage2 is $percentage4");
+
+  // String age1 = "10";
+  // String age2 = "20";
+  // int age3 = int.parse(age1);
+  // int age4 = int.parse(age2);
+  // int age5 = age3 + age4;
+  // print(age5);
+  // print(age3.isEven);
+  // print(age3.isOdd);
+  // print(age3.isFinite);
+  // print(age3.isInfinite);
+  // print(age3.isNegative);
+  // print(age3.isNaN);
+
+  // double percentage1 = 60;
+  // int percentage2 = 60;
+  // print(percentage1.compareTo(percentage2));
+  // int num1 = 13;
+  // int num2 = 11;
+  // int result = num1.remainder(num2);
+  // double num3 = num1.toDouble();
+  // int num4 = num3.toInt();
+  // String str = num4.toString();
+
+  // print(result);
+  // print(num3);
+  // print(num4);
+
+  // double percentage = 50.7;
+  // print(percentage.truncate());
+
+}
