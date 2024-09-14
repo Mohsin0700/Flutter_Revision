fdea7831aa3f783c8ecfe1b54e78f00a99fa4124
= <=>
2024-09-14 21:49:07 +0500

    Files are updated

--- class07_maps/main.dart
+++ class07_maps/main.dart
@@ -1,24 +1,31 @@
 void main(){
-  // Map<String, dynamic> userInfo = {
-    // "id": "abc12345",
-    // "name": "Mohsin",
-  //   "age": 27,
-  //   "city": "Karachi",
-  // };
-  // print(userInfo.keys);
-  // print(userInfo.values);
-  // userInfo["name"] = "Hafiz Mohsin"; // wrong method because we don't know if key exists or not?
-  // userInfo.putIfAbsent("name", ()=> "Hassan");
-  // print(userInfo);
-  // Map myInfo = {
-  //   "contact": 03433440700,
-  //   "cnic": 4210149356474
-  // };
-  // myInfo.addEntries(userInfo.entries);
-  // print(myInfo);
-  // myInfo.removeWhere((k, v)=> v.startsWith('K'));
-  // myInfo.removeWhere((k, v)=> k.startsWith('c'));
-  // print(myInfo);
+  Map<String, dynamic> userInfo = {
+    "id": "abc12345",
+    "name": "Mohsin",
+    "age": 27,
+    "city": "Karachi",
+  };
+  print(userInfo.keys);
+  print(userInfo.values);
+  userInfo["name"] = "Hafiz Mohsin"; // wrong method because we don't know if key exists or not?
+  userInfo.putIfAbsent("name", ()=> "Hassan");
+  print(userInfo);
+  Map myInfo = {
+    "contact": 03433440700,
+    "cnic": 4210149356474
+  };
+  myInfo.addEntries(userInfo.entries);
+  print(myInfo);
+  // myInfo.removeWhere((k, v)=> v.startsWith('k'));
+  // If we have different values rather than simple string then we can't use startsWith method with value
+  // Therefore I did some alteration methode to get the desired result in case of multiple data type of values
+  myInfo.removeWhere((key, value) {
+  if(value.runtimeType == String) {
+    return value.startsWith("K");
+  }
+  return key.startsWith('/');
+  },);
+  print(myInfo);
   
   // *********************************Merge_Methods********************************************
   // Map<int,String> fruit = {1: "Apple", 2: "Mango"};
@@ -29,9 +36,9 @@ void main(){
 
   // print(f3);
   // print(f4);
-  List<String> keys = ["name", "age", "email"];
-  List values = ["Hafiz Mohsin", 27, "muhammad786mohsin024@gmail.com"];
+  // List<String> keys = ["name", "age", "email"];
+  // List values = ["Hafiz Mohsin", 27, "muhammad786mohsin024@gmail.com"];
 
-  Map<String, dynamic> myInfo = Map.fromIterables(keys, values);
-  print(myInfo);
+  // Map<String, dynamic> myInfo = Map.fromIterables(keys, values);
+  // print(myInfo);
   }
