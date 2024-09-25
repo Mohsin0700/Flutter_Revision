529d01fd5439576ac83716323bc80808aeb2d7b9
= <=>
2024-09-25 17:41:56 +0500

    Files are updated

--- Oops_part_1/main.dart
+++ Oops_part_1/main.dart
@@ -1,19 +1,27 @@
 void main() {
   // we first crete class objects to access its memebers
-  Human obj1 = Human();
-  Human obj2 = Human();
-  obj1.name = "Arbab";
+  // Human obj1 = Human('Mohsin', 27);
+  // Human obj2 = Human("Arbab", 25);
+  Human obj = Human();
+  // obj1.name = "Arbab";
   // obj1.about();
   // print("${obj1.name} is a good coder");
   // print(obj1.about());
-  print(obj1.name);
-  print(obj2.name);
+  // print(obj1.name);
+  // print(obj2.name);
 }
 
 class Human {
   // These are called class members or fields
-  String name = "Mohsin";
-  int age = 27;
+  // String name = "Mohsin";
+  // int age = 27;
+  // String name;
+  // int age;
+  // Default Constructor
+  Human() {
+    print("Default constructor is called");
+  }
+  // Human(this.name, this.age);
 
   // These are called methods
   about() {
@@ -22,10 +30,10 @@ class Human {
   }
 
   eat() {
-    print("$name can eat");
+    // print("$name can eat");
   }
 
   drink() {
-    print("$name can drink");
+    // print("$name can drink");
   }
 }
---
+++ Oops_part_2/main.dart
@@ -0,0 +1,107 @@
+// void main() {
+// Whenever we create object of a class its constructor is called by default.
+// Student s1 = Student("Mohsin", 27);
+// }
+
+// class Student {
+//   String name;
+//   int age;
+//   // This keyword targets class attributes and its members
+//   Student(this.name, this.age) {
+//     showInfo();
+//   }
+
+//   showInfo() {
+//     print("Student name is: $name & his age is: $age");
+//   }
+// }
+
+// *************************************************************************************
+// **************************BASIC INHERITANCE******************************************
+// void main() {
+//   Teacher obj = Teacher();
+//   obj.name = "Riyasat";
+//   obj.age = 27;
+//   obj.showInfo();
+// }
+
+// class Person {
+//   String? name;
+//   int? age;
+
+//   showInfo() {
+//     print("My name is $name and I am $age years old");
+//   }
+// }
+
+// class Teacher extends Person {}
+
+// *******************************************************************************************
+// **************************CONSTRUCTOR INHERITANCE******************************************
+// void main() {
+//   Person obj1 = Person(name: "Mohsin", age: 27);
+//   obj1.showInfo();
+//   print(obj1.name);
+//   Teacher obj2 = Teacher(namee: "Muhammad Ali", agee: 60);
+//   obj2.showInfo();
+//   print(obj2.name);
+// }
+
+// class Person {
+//   late String name;
+//   late int age;
+
+//   Person({required this.name, required this.age}) {
+//     print("Parent class constructor called");
+//   }
+//   showInfo() {
+//     print("Hello my name is $name and I am $age year old");
+//   }
+// }
+
+// class Teacher extends Person {
+//   Teacher({required String namee, required int agee})
+//       : super(name: namee, age: agee) {
+//     print("Child class constructor called");
+//   }
+// }
+
+// *******************************************************************************************
+// ****************************************MIXINS*********************************************
+void main() {
+  Duck obj1 = Duck("Duck");
+  Fish obj2 = Fish("Fish");
+  Eagle obj3 = Eagle("Eagle");
+
+  obj1.swim(obj1.name);
+  obj1.fly(obj1.name);
+  obj2.swim(obj2.name);
+  obj3.fly(obj3.name);
+}
+
+mixin Swimming {
+  void swim(String name) {
+    print("$name can swim");
+  }
+}
+
+mixin Flying {
+  void fly(String name) {
+    print("$name can fly");
+  }
+}
+
+class Duck with Swimming, Flying {
+  late String name;
+  Duck(this.name);
+}
+
+class Fish extends Duck with Swimming {
+  late String name;
+  Fish(this.name) : super(name);
+}
+
+class Eagle extends Duck with Flying {
+  late String name;
+  Eagle(this.name) : super(name);
+}
